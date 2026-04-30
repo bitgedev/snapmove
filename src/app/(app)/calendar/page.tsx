93b6f20dbb99df07bb7bd77279e39b290f061a90
bitgedev <bitge.dev@gmail.com>
2026-04-30 20:18:50 +0900

    feat(calendar): implement CalendarGrid with workout date markers

--- src/app/(app)/calendar/page.tsx
+++ src/app/(app)/calendar/page.tsx
@@ -1,19 +1,34 @@
 "use client"
 
+import CalendarGrid from "@/components/calendar/CalendarGrid";
 import MonthNav from "@/components/calendar/MonthNav";
+import { getMockSessionsByMonth } from "@/lib/mock-data";
 import { useState } from "react";
 
 export default function CalendarPage() {
-    const [currentMonth, setCurrentMonth] = useState(new Date());
+    const [currentMonth, setCurrentMonth] = useState(
+        () => new Date()
+    );
+    const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+
     const handlePrev = () => {
         setCurrentMonth(new Date(currentMonth.getFullYear(), currentMonth.getMonth() - 1));
     }
     const handleNext = () => {
         setCurrentMonth(new Date(currentMonth.getFullYear(), currentMonth.getMonth() + 1));
     }
+    //CalendarGrid에서 클릭한 날짜를 넘겨받음
+    const handleSelectDate = (date:Date) => {
+        setSelectedDate(date);
+    }
+    //현재 달의 세션만 필터링해서 가져오기. getMonth는 0부터 시작하므로 +1하기
+    const sessions = getMockSessionsByMonth(currentMonth.getFullYear(), currentMonth.getMonth() + 1);                      
+    const workoutDates = new Set(sessions.map(s => s.date.slice(0, 10)));
+
     return (
         <main>
             <MonthNav currentMonth={currentMonth} onPrev={handlePrev} onNext={handleNext} />
+            <CalendarGrid currentMonth={currentMonth} workoutDates={workoutDates} selectedDate={selectedDate} onSelectDate={handleSelectDate}/>
         </main>
     );
 }
--- src/components/calendar/CalendarGrid.tsx
+++ src/components/calendar/CalendarGrid.tsx
@@ -1,3 +1,27 @@
-export default function CalendarGrid() {
-    
+interface CalendarGridProps {
+    currentMonth:Date;
+    workoutDates:Set<string>;
+    selectedDate:Date|null;
+    onSelectDate:(date:Date)=>void;
+}
+const dayOfTheWeek = ["일","월","화","수","목","금","토"];
+export default function CalendarGrid({currentMonth, workoutDates, selectedDate, onSelectDate}:CalendarGridProps) {
+    const year = currentMonth.getFullYear();
+    const month = currentMonth.getMonth();
+    const firstDayOfWeek = new Date(year, month, 1).getDay()
+    const lastDayOfMonth = new Date(year, month + 1, 0).getDate();
+    const cells = [
+        ...Array(firstDayOfWeek).fill(null),
+        ...Array.from({length:lastDayOfMonth}, (_, i) => i+ 1),
+    ];
+    return (
+        <div className="grid grid-cols-7">
+            {dayOfTheWeek.map((day)=> (
+                <div key={day}>{day}</div>
+            ))}
+            {cells.map((cell, index) => 
+                cell===null ? <div key={index}></div> : <div key={index}>{cell}</div>
+            )}
+        </div>
+    );
 }
